7a5bc7a4f52ac0545b47f8da4f9d6a441d80d228
aissatou7 <yayefallsaliou@gmail.com>
2023-04-28 17:54:19 +0000

    link

--- client/src/Component/Sidebar.tsx
+++ client/src/Component/Sidebar.tsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const Sidebar = () => {
   return (
     <div id="ContainerbtnSidebar">
-        <button id="btnSidebar"><Link to='Modifmdp'> Modification profil </Link></button>
+        <button id="btnSidebar"><Link to='Modifmdp' > Modification profil </Link></button>
         <button id="btnSidebar"><Link to='Donneconcerne'>  Données du conserné</Link></button>
         <button id="btnSidebar"><Link to='Sante'>  Santé</Link></button>
         <button id="btnSidebar"><Link to='Deplacement'>  Déplacement</Link></button>
