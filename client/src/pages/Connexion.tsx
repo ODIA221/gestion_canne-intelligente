5b3db2bf9c97e6187036a6650561566ed1a9fb51
odia221 <odia221@localhost.localdomain>
2023-04-30 04:55:12 +0000

    ctrl de saisie

--- client/src/pages/Connexion.tsx
+++ client/src/pages/Connexion.tsx
@@ -1,26 +1,69 @@
-import React from 'react'
+import React, { useState }from "react";
+import { useForm } from "react-hook-form";
 import "./Style2.css";
+import axios from "axios";
 
 export default function Connexion() {
+
+    /* Controle de saisie */
+    const [error, setErrror] =useState(null);
+
+    /*  hooks-form*/
+    const {
+        register,
+        formState: { errors },
+        handleSubmit,
+        } = useForm(
+        {mode:"onChange"}
+        );
+    /* btn ctrl saisie */
+    const onSubmit = (data: any) => console.log("");
+
+    const message="";
+
   return (
     
     <div className='Container'>
         <div id='titre'>Connexion</div>
-        <form className='connexion'>
+        <form className='connexion' onSubmit={handleSubmit(onSubmit)}>
             <div className="mb-3">
                 <label className="label">Id Canne</label>
-                <input type="email" 
+                <input 
                     className="form" 
                     id="form" 
                     aria-describedby="emailHelp"
+                    placeholder="Id-Canne" 
+                    {...register("id_canne", {
+                        required: "Champ Obligatoire",
+                        pattern:{
+                            value: /^[a-zA-Z]+[0-9-]+$/i,
+                            message: "Id incorrect",
+                        } 
+                    })}
                 />
+                {/* Message d'erreurs */}
+                {errors.id_canne && <small className='err'>{errors.id_canne.message }</small>}
             </div>
             <div className="mb-3">
                 <label  className="label">Mot de passe</label>
                 <input type="password" 
                     className="form" 
                     id="form"
+                    placeholder="Mot de passe" 
+                    {...register("password", {
+                        required: "Champ Obligatoire",
+                        minLength: {
+                        value: 5,
+                        message: "5 Caractètes au minimum"
+                        },
+                        maxLength: {
+                        value:10,
+                        message: "10 Caractètes au maximum"
+                        }
+                    })}
                 />
+                {/* Message d'erreurs */}
+                {errors.password && <small className='err'>{errors.password.message}</small>}
             </div>
             <button type="submit" id="btn">Connexion</button>
         </form>
